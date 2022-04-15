a92ed51986e1ac28d9664d83869309ef403adb98
luthfynur <nurluthfy98@gmail.com>
2022-04-16 02:28:38 +0700

    Fix vote button layout

--- src/components/Vote/index.js
+++ src/components/Vote/index.js
@@ -44,26 +44,30 @@ export default function Vote({ data, update, setUpdate, visibility, user }) {
     <div
       className={`${visibility} ${
         loading ? 'animate-pulse' : ''
-      } sm:mt-4 w-fit h-fit sm:h-[140px] flex pt-1 pl-4 pr-3 pb-1 flex-row gap-5 sm:pt-2 mt-1 sm:flex-col rounded-lg bg-lightGray text-moderateBlue font-rubik font-medium text-[20px]`}
+      } sm:mt-4 w-fit h-fit flex flex-row gap-0  mt-1 sm:flex-col rounded-lg bg-lightGray text-moderateBlue font-rubik font-medium text-[20px]`}
     >
-      <div className="w-[20px] h-[20px] pt-2" onClick={() => upVote()}>
+      <div
+        className={`w-[30px] h-[30px] p-2 rounded-t-lg rounded-b-lg hover:bg-black/20 mt-auto mb-auto  ${
+          user.currentUser !== null ? 'block' : 'hidden'
+        }`}
+        onClick={() => upVote()}
+      >
         <img
-          className={`h-[11px] mt-auto mb-auto ${
-            user.currentUser !== null ? 'block' : 'hidden'
-          }`}
+          className={`h-fit w-fit sm:ml-[1px]`}
           src={upVoteIcon}
           alt="plus"
         />
       </div>
 
-      <p className="sm:mb-5 sm:mt-7">
-        {data?.upVote.length - data?.downVote.length}
-      </p>
-      <div className="w-[20px] h-[20px] pt-3" onClick={() => downVote()}>
+      <p className="pr-2 pl-2">{data?.upVote.length - data?.downVote.length}</p>
+      <div
+        className={`w-[30px] h-[30px] pt-3 pl-2 rounded-t-lg rounded-b-lg hover:bg-black/20 mt-auto mb-auto ${
+          user.currentUser !== null ? 'block' : 'hidden'
+        }`}
+        onClick={() => downVote()}
+      >
         <img
-          className={`h-[3px] mt-auto mb-auto ${
-            user.currentUser !== null ? 'block' : 'hidden'
-          }`}
+          className={`h-fit w-fit sm:ml-[1px]`}
           src={downVoteIcon}
           alt="minus"
         />
